823a930e8c33c92655006f1e5c006290b5a1c020
Gabriel Thomas <gabriel.alphon@gmail.com>
2023-08-29 10:22:10 -0300

    frontend - config Ethers -> integrate smart contract in front-end

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -18,3 +18,8 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+// Contract Address deployed: 0x23908A11AF4ac69C18BF3C45a7b28fBc64d2F756
+
+// Successfully verified contract Voting on the block explorer.
+// https://mumbai.polygonscan.com/address/0x23908A11AF4ac69C18BF3C45a7b28fBc64d2F756#code
